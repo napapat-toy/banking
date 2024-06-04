f1e6196ef6592e6e82619451aa216545c1a3752d
toytoy08 <toy31327@gmail.com>
2024-06-05 04:18:05 +0700

    remove log

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import BankCard from "./BankCard";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
-  console.log(user);
-  
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
